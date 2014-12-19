2959118fcb0070403ff757e34fa3372faf7f545b
jasondyq <jasondyq@foxmail.com>
2014-12-19 16:41:06 +0800

    v1.0

--- weibo_robot.py
+++ weibo_robot.py
@@ -10,15 +10,15 @@ sys.setdefaultencoding('utf-8')
 
 #开发进度
 
-# 个人信息
+#个人信息
 __author__  = 'jas0ndyq'
 __email__   = 'jasondyq@foxmail.com'
 __version__ = 'v0'
 
-# 开启调试输出(0 or 1)
+#开启调试输出(0 or 1)
 debug = 0
 
-# 构造headers信息
+#构造headers信息
 user_agent = (
   'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.11 (KHTML, like Gecko) '
   'Chrome/20.0.1132.57 Safari/536.11'
@@ -27,7 +27,7 @@ session = requests.session()
 session.headers['User-Agent'] = user_agent
 session.headers['Host'] = 'api.weibo.com'
 
-# 设置全局变量
+#设置全局变量
 global api_key, api_secret, callback_url, userid, password
 api_key = '**********' # 请在微博开放平台获取
 api_secret = '**********' #请在微博开放平台获取
@@ -35,15 +35,15 @@ callback_url = 'http://jas0n.me'
 userid = 'xxx@xx.com' #微博登陆邮箱
 password = '**********' #微博登陆密码
 
-# 初始化API client
+#初始化API client
 global client, referer_url
 client =  APIClient(app_key=api_key, app_secret=api_secret, redirect_uri=callback_url)
 referer_url = client.get_authorize_url()
 if debug: print 'referer_url: %s' % referer_url
 
-# 获取回调地址的code
+#获取回调地址的code
 def get_code():
-  # 构造post数据
+  #构造post数据
   data = {
     'client_id': api_key,
     'redirect_uri': callback_url,
@@ -56,7 +56,7 @@ def get_code():
 
   session.headers['Referer'] = referer_url
 
-  # post数据到服务器
+  #post数据到服务器
   resp = session.post(
     url = 'https://api.weibo.com/oauth2/authorize',
     data = data
@@ -65,40 +65,40 @@ def get_code():
   if debug: print 'get url: %s' % resp.url
   if debug: print 'code is: %s' % resp.url[-32:]
   
-  # 截取回调url中的code
+  #截取回调url中的code
   code = resp.url[-32:]
   return code
 
 ###以上内容为配置阶段，相关配置服务均可参照此进行
 
-# 发文字微博
+#发文字微博
 def weibo_text(text):
-  # post构造的数据获取code
+  #post构造的数据获取code
   code = get_code()
 
-  # 获取授权令牌和期限
+  #获取授权令牌和期限
   token = client.request_access_token(code)
   client.set_access_token(token.access_token, token.expires_in)
 
-  # 发微博
+  #发微博
   client.statuses.update.post(status=text)
 
-# 发图片微博
+#发图片微博
 def weibo_pic(text, picture):
-  # post构造的数据获取code
+  #post构造的数据获取code
   code = get_code()
 
-  # 获取授权令牌和期限
+  #获取授权令牌和期限
   token = client.request_access_token(code)
   client.set_access_token(token.access_token, token.expires_in)
 
-  # 发图片微博
+  #发图片微博
   Pic = open(picture, 'url')
   client.statuses.upload.post(status=text, pic=Pic)
   Pic.close()
 
 
-#测试查询用户API
+#调用查询用户API
 def check_user(userid):
   code = get_code()
   token = client.request_access_token(code)
